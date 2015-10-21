d9ce82f69615a5775fa1ab53cb743d2ca02a1333
yashkrsingh <yashkr.singh@gmail.com>
2015-10-21 14:39:43 +0530

    Second Commit

--- Lab 5/Default Package/Maximum.java
+++ Lab 5/Default Package/Maximum.java
@@ -5,6 +5,10 @@
 */
 
 import java.util.Scanner;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.text.DateFormat;
+import java.util.Date;
 
 public class Maximum {
 	
@@ -18,10 +22,16 @@ public class Maximum {
 		return max;
 	}
 	
-	public static void main(String[] args) {
+	public static void main(String[] args) throws ParseException {
 		String[] stringarr = {"wacsd","was","waasdsdfasdf","rtyrty","sdfsdf","dfgsdfgsdf","fg","yhasdkfjhasdfkjhsadfkasd","sdfg","wretwertvzxcvb"};
 		Integer[] intarr = {3,4,5,1,6,8,9,4,1,0};
-		java.util.Date[] datearr= {09/03/1996};
+		String[] date = {"14/05/1995","12/12/1995","16/03/1997","05/08/1995","27/04/1987","04/06/2006","09/11/2011","08/08/2000","02/10/1843","09/09/2009"};
+		Date[] datearr = new Date[10];
+		
+		DateFormat f = new SimpleDateFormat("dd/MM/yyyy");
+		for(int i=0;i<10;i++){
+		datearr[i] = f.parse(date[i]);
+		}
 		
 		System.out.print("Displaying String Array contents: \n");
 		for(int i=0;i<stringarr.length;i++){
@@ -34,6 +44,12 @@ public class Maximum {
 			System.out.print(intarr[i]+"\n");
 		}
 		System.out.print("Maximum integer: "+findMaximum(intarr)+"\n");
+		
+		System.out.print("\nDisplaying Date Array contents: \n");
+		for(int i=0;i<datearr.length;i++){
+			System.out.print(datearr[i]+"\n");
+		}
+		System.out.print("Maximum date (Most recent): "+findMaximum(datearr)+"\n");
 		
 	}
 
